a89ff7f0192dccfd139b4173ceb6c1c315236cae
Kartik Chauhan <kartikchauhanforwork@gmail.com>
2026-03-25 11:22:56 +0530

    app file is modified so that it can fetch real cpu usage

--- app.py
+++ app.py
@@ -1,46 +1,20 @@
+# app.py
 from flask import Flask, render_template, jsonify
-import random
-import time
+from scaler import get_system_metrics
 
 app = Flask(__name__)
 
-active_servers = 2
-
-
 @app.route("/")
 def home():
+    # Serve the main dashboard
     return render_template("index.html")
-
 
 @app.route("/simulate")
 def simulate():
-
-    global active_servers
-
-    cpu = random.randint(20, 100)
-    processing_time = round(random.uniform(0.1, 1.5), 2)
-
-    if cpu > 75:
-        suggestion = "Horizontal Scaling Recommended"
-        active_servers += 1
-
-    elif cpu < 30 and active_servers > 1:
-        suggestion = "Scale Down Servers"
-        active_servers -= 1
-
-    else:
-        suggestion = "System Stable"
-
-    data = {
-        "cpu": cpu,
-        "processing_time": processing_time,
-        "servers": active_servers,
-        "suggestion": suggestion,
-        "time": int(time.time())
-    }
-
+    # Fetch system metrics from scaler.py
+    data = get_system_metrics()
     return jsonify(data)
 
-
 if __name__ == "__main__":
+    # Run Flask server on localhost:5000
     app.run(debug=True)
